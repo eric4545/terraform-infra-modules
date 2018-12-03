eab29152e0a973e1d459472485b5c9763ff9a4aa
Eric <eric4545@users.noreply.github.com>
2018-12-03 11:50:22 +0800

    :recycle: Refactor to use helm provider

--- flux/flux.tf
+++ flux/flux.tf
@@ -2,41 +2,76 @@ locals {
   flux_release_name = "${var.github_repo}-flux"
 }
 
-resource "null_resource" "install_flux" {
-  triggers {
-    chart_version = "${var.flux_chart_version}"
+resource "helm_repository" "weaveworks" {
+  name = "weaveworks"
+  url  = "https://weaveworks.github.io/flux"
+}
+
+resource "helm_release" "flux" {
+  name       = "${local.flux_release_name}"
+  repository = "${helm_repository.weaveworks.metadata.0.name}"
+  chart      = "flux"
+
+  namespace = "flux"
+  version   = "${var.flux_chart_version}"
+
+  set {
+    name  = "git.url"
+    value = "git@github.com:${var.github_repo_owner}/${var.github_repo}.git"
   }
 
-  provisioner "local-exec" {
-    command = <<SCRIPT
-helm repo add weaveworks https://weaveworks.github.io/flux && \
-helm upgrade ${local.flux_release_name} weaveworks/flux \
-    --set git.url="git@github.com:${var.github_repo_owner}/${var.github_repo}.git" \
-    --set git.user="${var.git_user}" \
-    --set git.email="${var.git_email}" \
-    --set git.branch=master \
-    --set git.path= \
-    --set git.ciSkip=true \
-    --set git.label="flux-sync" \
-    --set registry.rps="2" \
-    --set helmOperator.create=true \
-    --set helmOperator.git.chartsPath=charts \
-    --set helmOperator.tillerNamespace=${var.tiller_namespace} \
-    --version="${var.flux_chart_version}" \
-    --install \
-    --wait \
-    --namespace flux \
-    --tiller-namespace=${var.tiller_namespace}
-SCRIPT
+  set {
+    name  = "git.user"
+    value = "${var.git_user}"
+  }
+
+  set {
+    name  = "git.email"
+    value = "${var.git_email}"
+  }
+
+  set {
+    name  = "git.branch"
+    value = "master"
+  }
+
+  set {
+    name  = "git.ciSkip"
+    value = true
+  }
+
+  set {
+    name  = "git.label"
+    value = "flux-sync"
+  }
+
+  set {
+    name  = "registry.rps"
+    value = "2"
+  }
+
+  set {
+    name  = "helmOperator.create"
+    value = true
+  }
+
+  set {
+    name  = "helmOperator.git.chartsPath"
+    value = "charts"
+  }
+
+  set {
+    name  = "helmOperator.tillerNamespace"
+    value = "${var.tiller_namespace}"
   }
 }
 
 data "external" "flux_ssh_key" {
-  depends_on = ["null_resource.install_flux"]
+  depends_on = ["helm_release.flux"]
 
   program = ["/bin/sh", "-c",
     <<SCRIPT
-FLUX_POD=$(kubectl get pods -n flux -l "app=flux,release=${local.flux_release_name}" -o jsonpath="{.items[0].metadata.name}") && \
+FLUX_POD=$(kubectl get pods -n flux -l "app=flux,release=${helm_release.flux.name}" -o jsonpath="{.items[0].metadata.name}") && \
 SSH_KEY=$(kubectl -n flux logs $FLUX_POD | grep identity.pub | cut -d '"' -f2) && \
 echo "{\"key\":\"$SSH_KEY\"}"
 SCRIPT
