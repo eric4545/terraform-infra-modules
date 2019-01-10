bbb870064414c4251c6b949e9dcf91d7b02f0b68
Eric <eric4545@users.noreply.github.com>
2019-01-10 16:00:47 +0800

    🔒 Add more iam policy to worker/master Add nlb policy to master for istio Add policy to worker for cluster_autoscaler and route53

--- eks/master_iam.tf
+++ eks/master_iam.tf
@@ -1,5 +1,31 @@
-resource "aws_iam_role" "eks_admin_policy" {
-  name = "eks-admin-policy"
+resource "aws_iam_policy" "nlb_policy" {
+  name        = "nlb-policy"
+  path        = "/"
+  description = "In order to provision network load balancer."
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+   {
+      "Sid": "kopsK8sNLBMasterPermsRestrictive",
+      "Effect": "Allow",
+      "Action": [
+          "ec2:DescribeVpcs",
+          "ec2:CreateSecurityGroup",
+          "elasticloadbalancing:*"
+      ],
+      "Resource": [
+          "*"
+      ]
+  }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "eks_admin_role" {
+  name = "eks-admin-role"
 
   assume_role_policy = <<POLICY
 {
@@ -17,12 +43,17 @@ resource "aws_iam_role" "eks_admin_policy" {
 POLICY
 }
 
+resource "aws_iam_role_policy_attachment" "nlb_policy" {
+  policy_arn = "${aws_iam_policy.nlb_policy.arn}"
+  role       = "${aws_iam_role.eks_admin_role.name}"
+}
+
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.eks_admin_policy.name}"
+  role       = "${aws_iam_role.eks_admin_role.name}"
 }
 
 resource "aws_iam_role_policy_attachment" "service_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "${aws_iam_role.eks_admin_policy.name}"
+  role       = "${aws_iam_role.eks_admin_role.name}"
 }
--- eks/worker_iam.tf
+++ eks/worker_iam.tf
@@ -1,3 +1,87 @@
+resource "aws_iam_policy" "cluster_autoscaler" {
+  name        = "cluster-autoscaler-policy"
+  path        = "/"
+  description = "Certain resources and actions for cluster autoscaler"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeTags",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:TerminateInstanceInAutoScalingGroup"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "route53" {
+  name        = "route53-editor-policy"
+  path        = "/"
+  description = "Allow worker node to maintain route53 record"
+
+  policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Effect": "Allow",
+     "Action": [
+       "route53:ChangeResourceRecordSets"
+     ],
+     "Resource": [
+       "arn:aws:route53:::hostedzone/*"
+     ]
+   },
+   {
+     "Effect": "Allow",
+     "Action": [
+       "route53:ListHostedZones",
+       "route53:ListResourceRecordSets"
+     ],
+     "Resource": [
+       "*"
+     ]
+   }
+ ]
+}
+EOF
+}
+# Not sure the usage
+resource "aws_iam_policy" "logger" {
+  name        = "logger-policy"
+  path        = "/"
+  description = "Allow worker node to log"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": [
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        }
+    ]
+}
+EOF
+}
+
 resource "aws_iam_role" "worker_role" {
   name = "eks-worker-role"
 
@@ -17,12 +101,27 @@ resource "aws_iam_role" "worker_role" {
 POLICY
 }
 
-resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+resource "aws_iam_role_policy_attachment" "cluster_autoscaler" {
+  policy_arn = "${aws_iam_policy.cluster_autoscaler.arn}"
+  role       = "${aws_iam_role.worker_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "route53" {
+  policy_arn = "${aws_iam_policy.route53.arn}"
+  role       = "${aws_iam_role.worker_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "logger" {
+  policy_arn = "${aws_iam_policy.logger.arn}"
+  role       = "${aws_iam_role.worker_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = "${aws_iam_role.worker_role.name}"
 }
 
-resource "aws_iam_role_policy_attachment" "cni_policy" {
+resource "aws_iam_role_policy_attachment" "cni" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = "${aws_iam_role.worker_role.name}"
 }
