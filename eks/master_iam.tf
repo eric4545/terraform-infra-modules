e83c79a97980e4985b883aaa3a555661f7517bfc
Eric <eric4545@users.noreply.github.com>
2019-01-16 17:23:18 +0800

    🔧 Update master iam for istio manage NLB

--- eks/master_iam.tf
+++ eks/master_iam.tf
@@ -8,17 +8,35 @@ resource "aws_iam_policy" "nlb_policy" {
   "Version": "2012-10-17",
   "Statement": [
    {
-      "Sid": "kopsK8sNLBMasterPermsRestrictive",
       "Effect": "Allow",
       "Action": [
-          "ec2:DescribeVpcs",
           "ec2:CreateSecurityGroup",
-          "elasticloadbalancing:*"
+          "elasticloadbalancing:AddTags",
+          "elasticloadbalancing:CreateListener",
+          "elasticloadbalancing:CreateTargetGroup",
+          "elasticloadbalancing:DeleteListener",
+          "elasticloadbalancing:DeleteTargetGroup",
+          "elasticloadbalancing:DescribeListeners",
+          "elasticloadbalancing:DescribeLoadBalancerPolicies",
+          "elasticloadbalancing:DescribeTargetGroups",
+          "elasticloadbalancing:DescribeTargetHealth",
+          "elasticloadbalancing:ModifyListener",
+          "elasticloadbalancing:ModifyTargetGroup",
+          "elasticloadbalancing:RegisterTargets",
+          "elasticloadbalancing:SetLoadBalancerPoliciesOfListener"
       ],
       "Resource": [
           "*"
       ]
-  }
+  },
+    {
+      "Effect": "Allow",
+      "Action": [
+          "ec2:DescribeVpcs",
+          "ec2:DescribeRegions"
+      ],
+      "Resource": "*"
+    }
   ]
 }
 EOF
